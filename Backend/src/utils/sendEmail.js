fc90fede42e365e03afeb190ad470f3f792a752b
mayank18500 <mayank01082005@gmail.com>
2026-01-13 22:24:42 +0530

    email

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -14,7 +14,9 @@ const sendEmail = async (to, subject, html) => {
         }
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
